d25e2cf91e30a9f3236899cb4001bfd7cc743a6d
Craig Robson <craig.robson1@ncl.ac.uk>
2022-11-23 11:03:01 +0000

    Set paths and directories as variables

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
+# import required libraries
 import subprocess
 from os.path import isfile, join, isdir
 from os import listdir, remove, mkdir
 
+# set data path and directory names
 data_path = '/data'
+inputs_directory = 'inputs'
+input_data_directory = 'layers'
+temp_directory = 'temp'
+outputs_directory = 'outputs'
 
-if isdir(join(data_path,'temp')) is False:
-    mkdir(join(data_path,'temp'))
+
+# check if required folder structure in place
+# if so and folders have files in, empty
+## temp
+if isdir(join(data_path, temp_directory)) is False:
+    mkdir(join(data_path, temp_directory))
 else:
-    files = [f for f in listdir(join(data_path,'temp')) if isfile(join(data_path,'temp',f))]
+    files = [f for f in listdir(join(data_path, temp_directory)) if isfile(join(data_path, temp_directory,f))]
     for file in files:
-        remove(join(data_path,'temp',file))
+        remove(join(data_path, temp_directory,file))
 
-
-if isdir(join(data_path,'outputs')) is False:
-    mkdir(join(data_path,'outputs'))
+## outputs
+if isdir(join(data_path, outputs_directory)) is False:
+    mkdir(join(data_path, outputs_directory))
 
 # empty output dir
-files = [f for f in listdir(join(data_path,'outputs')) if isfile(join(data_path,'outputs',f))]
+files = [f for f in listdir(join(data_path, outputs_directory)) if isfile(join(data_path, outputs_directory,f))]
 for file in files:
-    remove(join(data_path,'outputs',file))
+    remove(join(data_path, outputs_directory,file))
 
 #get list of input files to loop through
+files = [f for f in listdir(join(data_path, inputs_directory, input_data_directory)) if isfile(join(data_path, inputs_directory, input_data_directory,f))]
+print('files to loop through: %s' %files)
 
-files = [f for f in listdir(join(data_path,'inputs','layers')) if isfile(join(data_path,'inputs','layers',f))]
-print('files to loop through')
-
+# loop through the files and process
 for file in files:
-    print(file)
-    file_name = file.split('.')[0]
-    subprocess.run(["gdalwarp", "-te", "0", "12000", "660000", "1212000", "-tr", "12000", "12000", "-r", "sum", join(data_path,'inputs', 'layers', file), join(data_path, 'temp','%s-%s.vrt'%(file_name,'temp') )])
+    print('Processing: %s' %file)
 
-    subprocess.run(["gdal_translate", "-of", "AAIGrid",join(data_path,"temp","%s-%s.vrt"%(file_name,'temp')), join(data_path,"outputs","%s-12km-sum.asc"%file_name)])
+    # get just the name of the file (remove the extension)
+    file_name = file.split('.')[0]
+
+    # re-grid to the 12km RCM grid, save as virtual raster in the temp directory
+    subprocess.run(["gdalwarp", "-te", "0", "12000", "660000", "1212000", "-tr", "12000", "12000", "-r", "sum", join(data_path, inputs_directory, input_data_directory, file), join(data_path, temp_directory,'%s-%s.vrt'%(file_name,'temp') )])
+
+    # save the virtual raster as an .asc in the output directory
+    subprocess.run(["gdal_translate", "-of", "AAIGrid",join(data_path, temp_directory,"%s-%s.vrt"%(file_name,'temp')), join(data_path, outputs_directory,"%s-12km-sum.asc"%file_name)])
